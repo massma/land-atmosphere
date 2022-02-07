08472090edcbc1d40db0ce1ebd0ea90bdaf5d2cf
Adam Massmann <akm2203@columbia.edu>
2022-02-07 09:04:29 -0500

    research stations I'm familiar with that have strong sample sizes

--- soundings.py
+++ soundings.py
@@ -4,42 +4,39 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+import collections as c
 
 sys.path.append(os.environ['CLASS4GL'])
 
 from interface_multi import stations,stations_iterator, records_iterator,get_record_yaml,get_records
 
-path = os.environ['CLASS4GL_DATA'] + '/forcing/IGRA_PAIRS_20190515/'
-path_output = os.environ['CLASS4GL_DATA'] + '/experiments/IGRA_PAIRS_20190515/BASE/'
+FORCING_PATH = os.environ['CLASS4GL_DATA'] + '/forcing/IGRA_PAIRS_20190515/'
 
+ALBANY_ID = 72518
 
-# loading station information
-# second argument is which subset of statiosn to initialize with; e.g., 'ini' or 'morning'
-# all_stations.table has a table (dataframe) of all the statiosn int he dataset
-all_stations = stations(path, suffix='ini', refetch_stations=False)
+SPOKANE_ID = 72786
+MILANO_ID = 16080
+NORMAN_ID = 72357
+ABERDEEN_ID = 72659
+SHREVEPORT_ID = 72248
+EDWARDS_ID = 72381
+LINCOLN_ID = 74560
+GAYLORD_ID = 72634
+PHOENIX_ID = 74626
+STERLING_ID = 72403
+GREENSBORO_ID = 72317
+BIRMINGHAM_ID = 72230
+PEACHTREE_ID = 72215
+PITTSBURGH_ID = 72520
 
-stations_iter = stations_iterator(all_stations)
-STNID,run_station = stations_iter.set_STNID(STNID=int(72518))
+STATION_ID = PITTSBURGH_ID
 
-# this is just a dataframe of all the stations we want to work on, we
-# could alternatiely load all stations with
-# pd.DataFrame(all_stations.table)
-all_stations_select = pd.DataFrame([run_station])
+def generic_path(station_id, suffix='yaml'):
+    """Generate a path given a STATION_ID and SUFFIX (e.g., yaml or pkl)."""
+    return "%s/%05d_%s.%s" % (FORCING_PATH, station_id, 'ini', suffix)
 
-# records morning is just a dataframe, which includes the indices of
-# each available sounding (index_start and index_end)
-# we could check and see if we can just get that index information from the _ini.pickle
-# file/dataframe
-records = get_records(all_stations_select,\
-                              path,\
-                              subset='ini',\
-                              refetch_records=False,\
-                              )
-records_station = records.query('STNID == ' + str(72518))
-path = "%s/%05d_%s.yaml" % (path, 72518, 'ini')
-f = open(path, 'r')
-
-for (STNID,chunk,index),record in records_station.iterrows():
+def load_record(f, record):
+    """Load the record given a file handle F and a RECORD from a *_ini.pkl dataframe"""
     c4gli = get_record_yaml(f,
                             record.index_start,
                             record.index_end,
@@ -52,6 +49,20 @@ for (STNID,chunk,index),record in records_station.iterrows():
         if var[:3] == 'adv':
             mean_adv_tropo = np.mean(profile_tropo[var+'_x']+profile_tropo[var+'_y'] )
             c4gli.update(source='era-interim',pars={var+'_tropo':mean_adv_tropo})
+    return c4gli
 
+
+f = open(generic_path(STATION_ID, suffix='pkl'), 'rb')
+df = pd.read_pickle(f)
+f.close()
+
+f = open(generic_path(STATION_ID, suffix='yaml'), 'r')
+
+records = c.deque()
+
+for (_station_id,_chunk,_index),record in df.iterrows():
+    records.append(load_record(f, record))
 
 f.close()
+
+print("\n****Length****:\n%10d" % len(records))
